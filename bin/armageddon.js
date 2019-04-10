4a857ea5f5f58ab4624886b1c94bb8b523e7d39c
Joel Tok <joel.toktc@axa.com.sg>
2019-04-10 17:05:46 +0800

    add check for target path argument

--- bin/armageddon.js
+++ bin/armageddon.js
@@ -4,6 +4,17 @@ var asteroid = require('./../lib/asteroid')
 var prompt = require('prompt')
 var yesno = require('yesno')
 var path = require('path')
+var fs = require('fs')
+
+const targetArg = process.argv[2];
+if (!targetArg) {
+  console.error('Please specify a target file or directory.')
+  process.exit()
+} else if (!fs.existsSync(targetArg)) {
+  console.error(`Target path does not exist: ${targetArg}.`)
+  process.exit()
+}
+const targetPath = path.resolve(targetArg);
 
 new Promise((resolve, reject) => {
 	yesno.ask('Are you sure you want to proceed? Use of this module could do irreversible damage to your code base if you are not careful. (Y/N)', true, function(ok) {
@@ -31,7 +42,7 @@ new Promise((resolve, reject) => {
 		console.log('Final confirmation. If you wish to proceed, please type in the name of your current working directory.')
 		prompt.start()
 		prompt.get(['name'], function(err, result) {
-			if (path.basename(process.cwd()) != result.name) {
+			if (path.basename(targetPath) != result.name) {
 				console.log('You did not enter the correct name for your current working directory')
 				process.exit()
 			} else {
@@ -42,7 +53,7 @@ new Promise((resolve, reject) => {
 })
 .then(() => {
 	console.log('ARMAGEDDON!!')
-	return asteroid.shower()
+	return asteroid.shower(targetPath)
 })
 .then(() => {
 	console.log('Did you survive?')
